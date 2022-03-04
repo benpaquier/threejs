6280671b5762aeaee93f4db79ffb7bb68773d7fd
benpaquier <hello@benpaquier.com>
2022-03-04 20:18:41 +0100

    My first house

--- ma-house/src/script.js
+++ ma-house/src/script.js
@@ -31,16 +31,19 @@ scene.add(gridHelper)
 
 // camera
 const camera = new THREE.PerspectiveCamera(45, aspectRatio, 0.1, 100)
-camera.position.set(7, 10, 10)
+camera.position.set(8, 12, 25)
 // camera.position.set(33, 7, 0)
 scene.add(camera)
 
 // debugger
-const gui = new dat.GUI()
+const gui = new dat.GUI({
+    width: 400
+})
 
 const guiFolders = {
     camera: gui.addFolder('Camera'),
-    roof: gui.addFolder('Roof')
+    roof: gui.addFolder('Roof'),
+    door: gui.addFolder('Door')
 }
 
 guiFolders.camera
@@ -87,42 +90,148 @@ gui
     .add(base.geometry.parameters, 'width')
 
 const roof = new THREE.Group()
-const panel1Geometry = new THREE.PlaneGeometry(10, 10)
+const panelLenght = Math.sqrt(Math.pow(baseHouseDimensions.width / 2, 2) + Math.pow(baseHouseDimensions.width / 2, 2))
+const panel1Geometry = new THREE.PlaneGeometry(panelLenght, baseHouseDimensions.depth)
 const panel1Material = new THREE.MeshBasicMaterial({
     color: 'brown',
     side: THREE.DoubleSide
 })
 
 const panel1 = new THREE.Mesh(panel1Geometry, panel1Material)
-panel1.position.y = 2
+const panel2 = new THREE.Mesh(panel1Geometry, panel1Material)
+panel1.position.set(-2, 4, 0)
+panel1.rotation.set(Math.PI / 2, Math.PI / 4, 0)
 roof.add(panel1)
+
+panel2.position.set(2, 4, 0)
+panel2.rotation.set(Math.PI / 2, -(Math.PI / 4), 0)
+roof.add(panel2)
+
+
+const frontPanelPositions = new Float32Array([
+    -4, 2, 5,
+    4, 2, 5,
+    0, 6, 5
+])
+
+const frontPanelAttribute = new THREE.BufferAttribute(frontPanelPositions, 3)
+const frontPanelPosition = new THREE.BufferGeometry()
+frontPanelPosition.setAttribute('position', frontPanelAttribute)
+const material = new THREE.MeshBasicMaterial({
+    color: "green",
+    side: THREE.DoubleSide
+})
+const frontPanel = new THREE.Mesh(frontPanelPosition, material)
+roof.add(frontPanel)
+
+const backPanelPositions = new Float32Array([
+    -4, 2, -5,
+    4, 2, -5,
+    0, 6, -5
+])
+
+const backPanelAttribute = new THREE.BufferAttribute(backPanelPositions, 3)
+const backPanelPosition = new THREE.BufferGeometry()
+backPanelPosition.setAttribute('position', backPanelAttribute)
+const backPanel = new THREE.Mesh(backPanelPosition, material)
+roof.add(backPanel)
 house.add(roof)
 
-const panel1Folder = guiFolders.roof.addFolder('panel1')
-panel1Folder
-    .add(panel1.geometry.parameters, 'width')
+const doorGeometry = new THREE.PlaneGeometry(1.2, 2)
+const doorMaterial = new THREE.MeshBasicMaterial({
+    color: 'red'
+})
+const door = new THREE.Mesh(doorGeometry, doorMaterial)
+door.position.set(0, -1, 5.01)
+house.add(door)
+
+guiFolders.door
+    .add(door.scale, 'x')
+    .min(0.5)
+    .max(3)
+    .step(0.1)
+    .name("Door width")
+
+guiFolders.door
+    .add(door.scale, 'y')
+    .min(0.5)
+    .max(3)
+    .step(0.1)
+    .name("Door height")
+
+
+const cylindreGeometry = new THREE.CylinderGeometry(0.1, 0.1, baseHouseDimensions.depth, 32)
+const cylindreMaterial = new THREE.MeshBasicMaterial({ color: 'yellow' })
+const cylindre = new THREE.Mesh(cylindreGeometry, cylindreMaterial)
+cylindre.position.set(0, 6, 0)
+cylindre.rotation.x = Math.PI / 2
+house.add(cylindre)
+
+const chemneyGeometry = new THREE.BoxGeometry(0.5, 2.5, 0.5) 
+const chemneyMaterial = new THREE.MeshBasicMaterial({ color: 'blue' })
+const chemney = new THREE.Mesh(chemneyGeometry, chemneyMaterial)
+chemney.position.set(2, 4, 4)
+house.add(chemney)
+
+
+guiFolders.roof
+    .add(panel1.scale, 'x')
     .min(0)
     .max(baseHouseDimensions.depth)
-    .name('panel1')
+    .step(0.1)
+    .name('panel1 width')
 
-panel1Folder
-    .add(panel1.geometry.parameters, 'height')
+guiFolders.roof
+    .add(panel1.scale, 'y')
     .min(0)
-    .max(10)
-    .name('panel1')
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 height')
 
-panel1Folder
+guiFolders.roof
     .add(panel1.rotation, 'x')
     .min(0)
-    .max(10)
-    .name('panel1X')
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 rotation x')
 
-panel1Folder
+guiFolders.roof
     .add(panel1.rotation, 'y')
     .min(0)
-    .max(10)
-    .name('panel1Y')
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 rotation y')
 
+guiFolders.roof
+    .add(panel1.rotation, 'z')
+    .min(0)
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 rotation z')
+
+guiFolders.roof
+    .add(panel1.position, 'x')
+    .min(-baseHouseDimensions.depth)
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 position x')
+
+guiFolders.roof
+    .add(panel1.position, 'y')
+    .min(-baseHouseDimensions.depth)
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 position y')
+
+guiFolders.roof
+    .add(panel1.position, 'z')
+    .min(-baseHouseDimensions.depth)
+    .max(baseHouseDimensions.depth)
+    .step(0.1)
+    .name('panel1 position z')
+
+// const helper = new THREE.PlaneHelper( panel1Geometry, 1, 0xffff00 );
+// scene.add(helper)
 
 camera.lookAt(base.position)
 
